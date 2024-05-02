f49be46c5b4f8b01bb540237805b68f2469dc662
Krockseed <66515313+Krockseed@users.noreply.github.com>
2024-05-02 17:18:27 +0900

    UPDATE 5.2 study

--- src/main/java/study/kksc/controller/BoardController.java
+++ src/main/java/study/kksc/controller/BoardController.java
@@ -1,14 +1,16 @@
 package study.kksc.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController("/board")
+@Slf4j
 public class BoardController {
-    @GetMapping("/")
-    public String createPost(@RequestParam String param) {
-        return null;
-    }
+//    @GetMapping("/")
+//    public String createPost(@RequestParam String param) {
+//        log.info("connect");
+//        return null;
+//    }
 }
